8c425c3f706336bed79ad4ca3e9ac218263d20e1
Peter Bognar <bognarpaeter@gmail.com>
2021-08-15 20:25:17 +0200

    added task 3

---
+++ assignment-2/task3/task_3_version_b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <omp.h>
+
+int omp_tasks(int v)
+{
+  printf("omp_tasks is called\n");
+  int a, b;
+  if( v <= 2 ) {
+    return 1;
+  } else {
+    #pragma omp task shared(a,b)
+    a = omp_tasks(v-1);
+    #pragma omp task shared(a,b)
+    b = omp_tasks(v-2);
+    #pragma omp taskwait
+    return a + b;
+  }
+}
+
+void main() {
+  int res;
+  omp_set_num_threads(4);
+  #pragma omp parallel
+  {
+    res = omp_tasks(5);
+  }
+  printf("res=%d\n", res);
+}
+
