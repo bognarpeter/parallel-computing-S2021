e89cf91f215029cee763a4e0c6dd184cca06edc8
Peter Bognar <bognarpaeter@gmail.com>
2021-08-15 20:26:06 +0200

    added filter

--- assignment-2/task4/julia-student-1.0.0-Source/julia-student-1.0.0-Source/src/filter.c
+++ assignment-2/task4/julia-student-1.0.0-Source/julia-student-1.0.0-Source/src/filter.c
@@ -1,7 +1,4 @@
-/*
- * (C) 2021 Sascha Hunold
- */
-
+#include <omp.h>
 #include <math.h>
 #include "filter.h"
 
@@ -9,14 +6,26 @@ const int fwidth2  = 1;
 const int fheight2 = 1;
 
 void apply_filter(png_bytep *row_pointers, int width, int height, double filter[3][3], int rounds) {    
-
-    for(int r=0; r<rounds; r++) {        
-        for(int i=fwidth2; i<width - fwidth2; i++) {        
-            for(int j=fheight2; j<height - fheight2; j++) {        
-                filter_on_pixel(row_pointers, i, j, filter);
-            }
-        }
-    }
-
+int i=0;
+int j=0;
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+		    for(int r=0; r<rounds; r++) 
+		    { 
+			#pragma omp task shared(i)	     
+		        for( i=fwidth2; i<width - fwidth2; i++) 
+			{        
+		            	#pragma omp task shared(j)
+				for( j=fheight2; j<height - fheight2; j++) 
+				{        
+					filter_on_pixel(row_pointers, i, j, filter);
+					
+				}
+		        }
+		    }
+		}
+	}
 }
 
